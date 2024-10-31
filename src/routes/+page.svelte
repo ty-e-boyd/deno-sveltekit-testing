4afe858aa76c408b564920cc63b1c535febf609a
Tyler Boyd <ty@tyboyd.dev>
2024-10-31 16:18:45 -0500

    Initial Commit
    This is the first commit of the repo, also testing out lazy git
    
    Newer Commit, To Test Squashing
    
    Going to use this commit to test squashing. This is the newer of the two
    commits.
    
    The Third Commit
    
    this is to test lazygit squashing and pulling

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    let count = $state(0);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
+
+<br />
+
+<p>
+    You have pressed <button
+        class="font-bold cursor-pointer"
+        onclick={() => {
+            count++;
+        }}>THIS TEXT</button
+    >
+    a total of {count} times
+</p>
+
+<br />
+
+<p>Adding this for the commit</p>
+<p>Adding this for the commit, but a second time.</p>
+
